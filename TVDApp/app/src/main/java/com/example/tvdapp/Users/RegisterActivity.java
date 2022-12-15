17732ae0ba976a2da34348d4e3894a8fa51e8c44
Midori2509 <midoringuyen01658110524@gmail.com>
2022-12-15 19:53:40 +0700

    UI_Register

---
+++ TVDApp/app/src/main/java/com/example/tvdapp/Users/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.tvdapp.Users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tvdapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
